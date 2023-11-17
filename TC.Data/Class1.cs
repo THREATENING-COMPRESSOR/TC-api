9d70c39e09d254cb0159446c2d8158d2ef349e4b
Robby <gavigan.4@osu.edu>
2023-11-16 21:59:21 -0500

    db context

--- TC.Data/Class1.cs
+++ TC.Data/Class1.cs
@@ -1,6 +1,19 @@
-﻿namespace TC.Data;
+﻿using TC.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
+// Specifies the namespace in which the class is declared
+namespace TC.Data
 {
+    // The 'StoreContext' class serves as a data context for database operations
+    public class StoreContext : DbContext
+    {
+        // Constructor for the 'StoreContext' class that takes in 'DbContextOptions'
+        public StoreContext(DbContextOptions<StoreContext> options)
+            // Calls the base class constructor with the provided 'options'
+            : base(options)
+        { }
 
+        // Represents a database table 'Item' in the context
+        public DbSet<Item> Items { get; set; }
+    }
 }
