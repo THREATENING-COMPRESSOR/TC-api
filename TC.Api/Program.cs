d758f413ec4b1ebf7d92161e0e399b1f4c22b414
Robby <gavigan.4@osu.edu>
2023-11-17 00:01:34 -0500

    configure api

--- TC.Api/Program.cs
+++ TC.Api/Program.cs
@@ -1,9 +1,17 @@
+using TC.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    b => b.MigrationsAssembly("TC.Api"))
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
