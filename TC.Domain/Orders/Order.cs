f96f385536aa92e7a9e94c045426906e4dab17bd
Robby <gavigan.4@osu.edu>
2023-11-20 12:51:55 -0500

    fixed issue

--- TC.Domain/Orders/Order.cs
+++ TC.Domain/Orders/Order.cs
@@ -5,8 +5,8 @@ namespace TC.Domain.Orders
 {
     public class Order
     {
-        public int Id {get; set; }
-        public List<OrderItem> Items { get;set; }
+        public int Id { get; set; }
+        public List<OrderItem> Items { get; set; }
         public DateTime CreatedDate { get; set; }
         public decimal TotalPrice => Items.Sum(i => i.Price);
         
