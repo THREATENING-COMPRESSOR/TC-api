1475658443ea1f0af536c45cec3412f38fa9bbe7
Robby <gavigan.4@osu.edu>
2023-11-23 12:01:41 -0500

    last test complete

--- Tests/TC.Domain.Tests/RatingTests.cs
+++ Tests/TC.Domain.Tests/RatingTests.cs
@@ -27,4 +27,33 @@ public class RatingTests
         var rating = new Rating(0, "Mike", "Great fit!");
 
     }
+
+    [TestMethod]
+    public void Can_Create_New_Item()
+    {
+        // arrange
+        var item = new Item("Chicken", "Is chicken", "KFC", 15.99m);
+
+        // assert
+        Assert.AreEqual("Chicken", item.Name);
+        Assert.AreEqual("Is chicken", item.Description);
+        Assert.AreEqual("KFC", item.Brand);
+        Assert.AreEqual(15.99m, item.Price);
+
+    }
+
+        [TestMethod]
+
+    public void Can_Create_Add_Rating()
+    {
+        // arrange
+        var item = new Item("Chicken", "Is chicken", "KFC", 15.99m);
+        var rating = new Rating(5, "Dave", "The chicken is dry");
+
+        // add to item
+        item.AddRating(rating);
+
+        // assert
+        Assert.AreEqual(item.Ratings[0], rating);
+    }
 }
